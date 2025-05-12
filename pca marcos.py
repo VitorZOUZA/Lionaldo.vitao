1d9878b236618f43610ccc664d2c2a61ac17d128
VitorZOUZA <pvaraujo2007@gmail.com>
2025-05-12 08:50:29 -0300

    Update pca marcos.py

--- pca marcos.py
+++ pca marcos.py
@@ -1,4 +1,8 @@
 import analisar_numeros
 analisar_numeros.media
-print(media)
+
+print("Média:", resultado["média"])
+print("Maior número:", resultado["maior"])
+print("Menor número:", resultado["menor"])
+print("Quantidade de números pares:", resultado["quantidade_pares"])
 
