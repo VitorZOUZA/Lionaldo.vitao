26d8fd00ef919cb805ae155b0cfecf1b61b21772
VitorZOUZA <pvaraujo2007@gmail.com>
2025-05-12 08:51:14 -0300

    Update trabalho pca.py

--- trabalho pca.py
+++ trabalho pca.py
@@ -13,8 +13,3 @@ def analisar_numeros(numeros):
 
 numeros = [10, 23, 5, 8, 12, 33, 42, 7, 19, 28, 3, 16, 9, 50, 21]
 resultado = analisar_numeros(numeros)
-
-print("Média:", resultado["média"])
-print("Maior número:", resultado["maior"])
-print("Menor número:", resultado["menor"])
-print("Quantidade de números pares:", resultado["quantidade_pares"])
